bdd1abea3578f6d3c0f224d17a9f21858c895ecd
5010FHS5 <roboticsteam.fhs5@gmail.com>
2017-01-19 18:04:37 -0500

    Initial commit of drivetrain
    Initial commit of drivetrain chassis code.

---
+++ SteamWorks2017/src/org/usfirst/frc/team5010/robot/subsystems/DriveTrainMain.java
@@ -0,0 +1,39 @@
+package org.usfirst.frc.team5010.robot.subsystems;
+
+import org.usfirst.frc.team5010.robot.RobotMap;
+import org.usfirst.frc.team5010.robot.commands.TankDrive;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+
+/**
+ *
+ */
+public class DriveTrainMain extends Subsystem {
+
+	SpeedController rightMotor = RobotMap.driveMotorRight;
+	SpeedController leftMotor = RobotMap.driveMotorLeft;
+	Gyro gyro = RobotMap.gyro;
+	AnalogInput distance = RobotMap.distance;
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+    	setDefaultCommand(new TankDrive());
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    public void driveStraightandSafe(double power) {
+    	rightMotor.set(power);
+    	leftMotor.set(-power);
+    	
+    }
+    
+    public void drive(double leftPower, double rightPower) {
+    	rightMotor.set(rightPower);
+    	leftMotor.set(-leftPower);
+    }
+}
+
