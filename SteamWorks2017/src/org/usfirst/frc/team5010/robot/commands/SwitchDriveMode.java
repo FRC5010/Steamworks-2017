4fe1c494ae30a67a90cf29b2a10db2773779fdde
5010TigerDynasty <roboticsteam.fhs@gmail.com>
2017-02-04 14:00:39 -0500

    Switching drive modes

---
+++ SteamWorks2017/src/org/usfirst/frc/team5010/robot/commands/SwitchDriveMode.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team5010.robot.commands;
+
+import org.usfirst.frc.team5010.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+
+/**
+ *
+ */
+public class SwitchDriveMode extends Command {
+
+    public SwitchDriveMode() {
+        
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	if(Robot.forwardOrReverse){
+    		Scheduler.getInstance().add(new TankDriveReverse());
+    	}else{
+    		Scheduler.getInstance().add(new TankDriveForward());
+    	}
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
